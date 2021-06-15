1e8525ab11bfaadf506fc4a246f61e3b12575545
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-06-15 23:25:35 +0100

    updated allurdecors.py

--- allurdecors.py
+++ allurdecors.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+  
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2523b14a259b1e38d321f17132f5fcf8'
 
 @app.route("/")
 def index():
@@ -9,6 +12,25 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/register/", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account successfully create for {form.username.data}', 'success')
+        return redirect(url_for('login'))
+    return render_template("register.html", title='register', form=form)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():   
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == '1234':
+            flash(f'Login successful for {form.email.data}', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash(f'Login unsuccessful check username or password', 'danger')
+
+    return render_template("login.html", title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
