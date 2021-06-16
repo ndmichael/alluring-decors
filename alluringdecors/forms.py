c1ae7e87741dce9f41f1be30ee1a77b8f1e8ebb9
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-06-16 15:42:42 +0100

    updated forms added username and email validation

--- alluringdecors/forms.py
+++ alluringdecors/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from alluringdecors.models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email , EqualTo
+from wtforms.validators import (DataRequired, Length, Email , EqualTo, ValidationError)
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
@@ -9,6 +10,15 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register Now')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username taken please try another')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email eixted already!!!')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
