7d0cd6827cc38a42552aed98a5a0d9570ef8b46d
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-06-21 09:42:19 +0100

    updated service model

--- alluringdecors/models.py
+++ alluringdecors/models.py
@@ -7,6 +7,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -14,11 +15,13 @@ class User(db.Model, UserMixin):
     is_staff = db.Column(db.Boolean,)
     is_active = db.Column(db.Boolean,)
     feedbacks = db.relationship('Feedback', backref='feedback', lazy=True)
+    requests = db.relationship('Service_request', backref='service_request', lazy=True)
 
     def __repr__(self):
         return f'{self.username}'
 
 class Category_project(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), unique=True, nullable=False)
     projects = db.relationship('Project', backref='project', lazy=True)
@@ -26,7 +29,9 @@ class Category_project(db.Model):
     def __repr__(self):
         return f'{self.name}'
 
+
 class Project(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
     client = db.Column(db.String(40), nullable=True)
@@ -40,6 +45,7 @@ class Project(db.Model):
 
 
 class FAQ(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(150), nullable=False)
     answer = db.Column(db.String(400), nullable=False)
@@ -62,6 +68,7 @@ class Feedback(db.Model):
 
 
 class Contact(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     detail = db.Column(db.String(150), nullable=False)
@@ -69,3 +76,18 @@ class Contact(db.Model):
 
     def __repr__(self):
         return f'{self.title}'
+
+
+class Service_request(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20), nullable=False)
+    domain = db.Column(db.String(200), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    remark = db.Column(db.String(200), nullable=True)
+    status = db.Column(db.String(), nullable=True, default="recieved")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_sent= db.Column(db.DateTime(200), nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'{self.type}'
